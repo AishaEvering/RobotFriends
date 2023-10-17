a4afc9685b1115ccee7d620b6b1af8d17b6b7a7e
Aisha Evering <shovon3000g@gmail.com>
2023-10-17 02:09:52 -0700

    create robot friend app

---
+++ src/components/CardList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({ robots }) => {
+  return (
+    <>
+      {robots.map((bot) => {
+        return (
+          <Card key={bot.id} id={bot.id} name={bot.name} email={bot.email} />
+        );
+      })}
+    </>
+  );
+};
+
+export default CardList;
